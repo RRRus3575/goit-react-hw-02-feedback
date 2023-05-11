3a6e9bfdab98c2152822b0d054519bfc9878792f
Ruslan <ruslan.grudistov@gmail.com>
2023-05-11 15:40:39 +0300

    123

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,7 +2,7 @@ import css from './FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(btn => (
-    <button key={btn} onClick={onLeaveFeedback} name={btn}>
+    <button key={btn} onClick={onLeaveFeedback} name={btn} className={css.btn}>
       {btn}
     </button>
   ));
